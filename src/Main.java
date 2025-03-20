a8b8c04232de7c62070e78bbcfc646a253c3076b
Nikolai Milidi <mmiqizy@gmail.com>
2025-03-20 16:48:27 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,49 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-            String result = calc("3 - 5");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите арифметическую операцию: ");
+        String input = scanner.nextLine();
+        try {
+            String result = calc(input);
             System.out.println(result);
+        } catch (IllegalArgumentException | ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 
     public static String calc(String input) {
         String[] parts = input.split(" ");
 
-        if (parts.length != 3) {
-            throw new IllegalArgumentException("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+        if (parts.length < 3) {
+            throw new IllegalArgumentException("throws Exception //т.к. строка не является математической операцией");
         }
+
+        if (parts.length != 3) {
+            throw new IllegalArgumentException("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+        }
+
 
         int num1 = Integer.parseInt(parts[0]);
         int num2 = Integer.parseInt(parts[2]);
 
-        if (num1 < 1 || num1 > 10 || num2 < 0 || num2 > 10) {
+        if (num2 == 0) {
+            throw new ArithmeticException("Деление на 0 невозможно");
+        }
+
+        if (num1 < 1 || num1 > 10 || num2 < 1 || num2 > 10) {
             throw new IllegalArgumentException("Числа должны быть от 1 до 10");
         }
 
-        int result = 0;
-
-        switch (parts[1]) {
-            case "+":
-                result = num1 + num2;
-                break;
-            case "-":
-                result = num1 - num2;
-                break;
-            case "*":
-                result = num1 * num2;
-                break;
-            case "/":
-                if (num2 == 0) {
-                    throw new ArithmeticException("Деление на 0 невозможно");
-                }
-                result = num1 / num2;
-                break;
-            default:
-                throw new IllegalArgumentException("Неверный формат математической операции");
-        }
+        int result = switch (parts[1]) {
+            case "+" -> num1 + num2;
+            case "-" -> num1 - num2;
+            case "*" -> num1 * num2;
+            case "/" -> num1 / num2;
+            default -> throw new IllegalArgumentException("Неверный формат математической операции");
+        };
 
 
         return "Результат: " + result;
